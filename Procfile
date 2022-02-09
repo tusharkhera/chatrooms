2664a9df81cc603e835f7659a312d4ca180f9133
tusharkhera <63866445+tusharkhera@users.noreply.github.com>
2022-02-09 15:16:28 +0530

    save

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: daphne chatrooms.asgi:application --port $PORT --bind 0.0.0.0
+web: daphne -b 0.0.0.0 -p $PORT chatrooms.asgi:application
--- chatrooms/asgi.py
+++ chatrooms/asgi.py
@@ -1,17 +1,20 @@
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
-import myapp.routing
-django_asgi_app = get_asgi_application()
-import django
-from channels.auth import AuthMiddlewareStack
-
+from django.conf.urls import url
+ 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatrooms.settings')
-django.setup()
-
+ 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+ 
+from channels.auth import AuthMiddlewareStack   
+import myapp.routing
+ 
+ 
 application = ProtocolTypeRouter({
-    'http' : django_asgi_app,
-    'websocket' : AuthMiddlewareStack(URLRouter(
+    "http": django_asgi_app,
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
             myapp.routing.websocket_urlpatterns
         )
     )
